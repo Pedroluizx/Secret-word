273a2bb15d52df6d25d262d61941c29dd69dde9a
Pedro Luiz <pedroluizalbx@hotmail.com>
2022-07-11 21:27:53 -0400

    First Commit

---
+++ secretword/src/data/Words.js
@@ -0,0 +1,43 @@
+export const wordsList = {
+  carros: [
+    'Gol',
+    'Palio',
+    'Celta',
+    'Onix',
+    'Hb20',
+    'Ferrari',
+    'Corolla',
+    'Hilux'
+  ],
+  frutas: ['Mamão', 'Uva', 'maçã', 'Laranja', 'Morango'],
+  corpo: [
+    'Braço',
+    'Perna',
+    'Olho',
+    'Barriga',
+    'Cabeça',
+    'Ombro',
+    'Joelho',
+    'Pé',
+    'Mão'
+  ],
+  computador: ['Teclado', 'Mouse', 'Monitor', 'Cpu', 'Memoria'],
+  cidades: [
+    'Manaus',
+    'Itacoatiara',
+    'Manacapuru',
+    'Iranduba',
+    'RiodeJaneiro',
+    'Sãopaulo'
+  ],
+  alimento: [
+    'Arroz',
+    'Macarrão',
+    'Ovo',
+    'Feijão',
+    'kikão',
+    'Esfirra',
+    'Farofa',
+    'Feijoada'
+  ]
+}
