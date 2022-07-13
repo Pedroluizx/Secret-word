fe9d9c9b129c52ce0084e09acc7d9d03acd88c98
Pedro Luiz <pedroluizalbx@hotmail.com>
2022-07-12 20:42:25 -0400

    First Screen

---
+++ secretword/src/components/StartScreen.js
@@ -0,0 +1,13 @@
+import './StartScreen.css'
+
+const StartScreen = () => {
+  return (
+    <div className="start">
+      <h1>Secret word</h1>
+      <p>Click no botão abaixo para jogar</p>
+      <button>Começar o jogo</button>
+    </div>
+  )
+}
+
+export default StartScreen
